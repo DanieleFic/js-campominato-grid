da815224a42d2877c73f49c1c1241cbb680b3501
DanieleFic <reb_ficus22@hotmail.it>
2021-12-20 19:02:45 +0100

    aggioustato il css

--- js/myscript.js
+++ js/myscript.js
@@ -8,7 +8,25 @@ con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
 
-let bottonePlay = document.getElementById("play")
+let bottonePlay = document.getElementById("play");
+
 bottonePlay.addEventListener("click" ,function(){
-    document.getElementById("mySelect").value
+    let scelta = document.getElementById("mySelect").value;
+
+    caselle.innerHTML = "";
+
+if(scelta == "facile"){
+    for(i=1; i<=100; i++){
+        caselle.innerHTML += `<div class="quadrato1 col-1 item-${i}">${i}</div>`;
+    }
+
+}else if(scelta == "media"){
+    for(i=1; i<=81; i++){
+        caselle.innerHTML += `<div class="quadrato2 col-1 item-${i}">${i}</div>`;
+    }
+}else
+    for(i=1; i<=50; i++){
+    caselle.innerHTML += `<div class="quadrato3 col-1 item-${i}">${i}</div>`;
+}
 });
+
